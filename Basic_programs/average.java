41786a6feaa4486f783257846005d126a8ffe15e
adityasaivit <113006142+adityasaivit@users.noreply.github.com>
2024-12-12 20:03:52 +0530

    Create average.java

---
+++ Basic_programs/average.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class average
+{
+  public static void main(String args[])
+  {
+    Scanner scan=new Scanner(System.in);
+
+    int a=scan.nextInt();
+    int b=scan.nextInt();
+    int c=scan.nextInt();
+
+    double avg=(a+b+c)/3.0;
+
+    System.out.println("Average is "+avg);
+
+
+
+  }
+}
