e9a021110872a891ba286d40794ab83e21e54f34
adityasaivit <113006142+adityasaivit@users.noreply.github.com>
2024-12-15 16:42:18 +0530

    Add files via upload

---
+++ BinaryTrees/POstordertraversal.java
@@ -0,0 +1,78 @@
+class Node
+{
+	int val;
+	Node left;
+	Node right;
+	public Node(int v)
+	{
+		this.val=v;
+		this.left=null;
+		this.right=null;
+	}
+}
+class Binarytree
+{
+	Node root;
+	public Binarytree()
+	{
+		this.root=null;
+	}
+	public void add(int v)
+	{
+		if(this.root==null)
+		{
+			this.root=new Node(v);
+		}
+		else{
+			Node temp=this.root;
+			while(true)
+			{
+				if(v<temp.val)
+				{
+					if(temp.left!=null)
+					{
+						temp=temp.left;
+					}
+					else{
+						temp.left=new Node(v);
+						break;
+					}
+				}
+				else{
+					if(temp.right!=null)
+					{
+						temp=temp.right;
+					}
+					else{
+						temp.right=new Node(v);
+						break;
+					}
+				}
+			}
+		}
+	}
+}
+public class Postordertraversal
+{
+	public static void postorder(Node root)
+	{
+		if(root!=null)
+		{
+			postorder(root.left);
+			postorder(root.right);
+			System.out.print(root.val);
+		}
+	}
+	public static void main(String args[])
+	{	
+
+		Binarytree b=new Binarytree();
+		b.add(5);
+		b.add(3);
+		b.add(8);
+
+		postorder(b.root);
+
+
+	}
+}
